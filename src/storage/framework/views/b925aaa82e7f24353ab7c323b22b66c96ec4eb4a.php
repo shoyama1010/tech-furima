4f0a24b671c0b354a6dd90517f715d70e540cbbf
Shoyama <kochisk8@gmail.com>
2025-02-11 21:53:00 +0900

    メール認証更新

--- src/storage/framework/views/b925aaa82e7f24353ab7c323b22b66c96ec4eb4a.php
+++ src/storage/framework/views/b925aaa82e7f24353ab7c323b22b66c96ec4eb4a.php
@@ -1,7 +1,5 @@
-@extends('layouts.app')
-
-<!-- @section('content') -->
-@section('main')
+<!-- <?php $__env->startSection('content'); ?> -->
+<?php $__env->startSection('main'); ?>
 <div class="container">
     <div class="email">
         <h1>メールアドレス認証</h1>
@@ -10,16 +8,18 @@
         <a href="http://localhost:8025" target="_blank" class="d-block text-center mt-3">認証リンクへ</a>
     </div>
 
-    @if (session('success'))
+    <?php if(session('success')): ?>
     <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-    @endif
+        <?php echo e(session('success')); ?>
 
-    <form method="POST" action="{{ route('verification.send') }}">
-        @csrf
+    </div>
+    <?php endif; ?>
+
+    <form method="POST" action="<?php echo e(route('verification.send')); ?>">
+        <?php echo csrf_field(); ?>
         <p>メールが届かない場合は、以下のボタンをクリックして再送してください。</p>
         <button type="submit" class="btn btn-primary">認証メールを再送する</button>
     </form>
 </div>
-@endsection
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/resources/views/auth/verify-email.blade.php ENDPATH**/ ?>
